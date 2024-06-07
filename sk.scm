5672602838f234bfc406ccd56aab0b3b06c024ad
XiangyuGuo <tobielf@gmail.com>
2024-06-06 18:15:42 -0700

    Add `has-negation?`
    Syntax analysis to find any negation (noto) in the program.

--- sk.scm
+++ sk.scm
@@ -1,1 +1,19 @@
 (load "mk.scm")
+
+
+;;; Syntax analysis to find any negation (noto) in the program.
+(define-syntax has-negation?
+  (syntax-rules (noto conde fresh)
+    ((_ (noto g)) #t)
+    ((_ (conde (g0 g ...) (g1 g^ ...) ...))
+        (or (has-negation? g0 g ...)
+            (has-negation? g1 g^ ...)
+            ...))
+    ((_ (fresh (x ...) g0 g ...))
+        (has-negation? g0 g ...))
+    ((_ g) #f)
+    ((_ g0 g1 ...) 
+        (or (has-negation? g0)
+            (has-negation? g1)
+            ...))
+))
