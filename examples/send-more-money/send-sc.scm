d7db57fef160467965885b0df24fbc80758d825b
Xiangyu Guo <tobielf@gmail.com>
2026-01-23 14:33:43 -0700

    Add SEND+MORE=MONEY puzzle
    This is another classic example of constraint satisfaction.
    
    The first solution, written in answer set programming (ASP), utilizes conflict-driven nogood learning (CDNL) to produce an answer in around 200 seconds. There is another encoding using the ASP choice rule to reduce it to around 18 seconds.
    
    The second solution, written in stableKanren without a heuristic, produces an answer in around 5000 seconds. Gradually introducing the heuristic improves performance to 15 seconds. (And some partial answers to less than 2 seconds)
    
    We use this example as a starting point to apply heuristic algorithms to stableKanren.

---
+++ examples/send-more-money/send-sc.scm
@@ -0,0 +1,26 @@
+(load "send.scm")
+
+; Break into smaller constraints.
+; :- assign(d, D), assign(e, E), assign(y, Y), (D + E) % 10 != Y.
+(constrainto [(assign 'd d) (assign 'e ev) (assign 'y yv)]
+             [(not (= yv (mod (+ d ev) 10)))])
+
+; (D + E) / 10 is the first carry bit, C1.
+; :- assign(d, D), assign(e, E), assign(n, N), assign(r, R), ((D + E) / 10 + N + R) % 10 != E.
+(constrainto [(assign 'd dv) (assign 'e ev) (assign 'n nv) (assign 'r rv)]
+             [(not (= ev (mod (+ nv rv (floor (/ (+ dv ev) 10))) 10)))])
+
+; (N + R + C1) / 10 is the second carry bit, C2.
+; :- assign(d, D), assign(n, N), assign(r, R), assign(e, E), assign(o, O), (((D + E) / 10 + N + R) / 10 + E + O) % 10 != N.
+(constrainto [(assign 'd dv) (assign 'n nv) (assign 'r rv) (assign 'e ev) (assign 'o ov)]
+             [(not (= nv (mod (+ ov ev (floor (/ (+ nv rv (floor (/ (+ dv ev) 10)) ) 10))) 10)))])
+
+; (E + O + C2) / 10 is the third carry bit, C3.
+; :- assign(e, E), assign(o, O), assign(s, S), assign(m, M), ((E + O) / 10 + S + M) % 10 != O.
+(constrainto [(assign 'n nv) (assign 'd dv) (assign 'r rv) (assign 'e ev) (assign 'o ov) (assign 's sv) (assign 'm mv)]
+             [(not (= ov (mod (+ mv sv (floor (/ (+ ev ov (floor (/ (+ nv rv (floor (/ (+ dv ev) 10)) ) 10)) ) 10))) 10)))])
+
+; (S + M + C3) / 10 is the fourth carry bit, C4.
+; :- assign(s, S), assign(m, M), (S + M) / 10 != M.
+(constrainto [(assign 'n nv) (assign 'd dv) (assign 'r rv) (assign 'e ev) (assign 'o ov) (assign 's sv) (assign 'm mv)]
+             [(not (= mv (floor (/ (+ sv mv (floor (/ (+ ev ov (floor (/ (+ nv rv (floor (/ (+ dv ev) 10)) ) 10)) ) 10))) 10))))])
